b304cfb4d6cf63ae99c7690a9048353ae0e06a51
Kenny <lli15@stuy.edu>
2018-11-27 14:07:40 -0500

    added login, register, authenticate, and database

--- app.py
+++ app.py
@@ -8,12 +8,58 @@ flash, session, url_for, redirect
 
 app = Flask(__name__)
 
-#root route
+app.secret_key = os.urandom(32)
+
+#---------- Login Routes ----------
+#Sends the user to the login page if they are logged in
+#If not they have to login or register
 @app.route("/")
+def login():
+    if "logged_in" in session:
+        return redirect(url_for("home"))
+    return render_template("login.html")
+
+#Authenticates user and creates a session
+@app.route("/auth")
+def authenticate():
+    if story.auth_user(request.form["user"], request.form["password"]):
+        session["logged_in"] = request.form["user"]
+        return redirect(url_for("home"))
+    else:
+        flash("username or password is incorrect")
+        return redirect(url_for("login"))
+
+#---------- Register Routes ----------
+#Sends the user to the register page to create account
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
+#Adds user to the database after they register
+#Flashes message accordingly if user exists or form isn't filled
+@app.route("/adduser")
+def add_user():
+    if(not request.form["user"].strip() or not request.form["password"] or not request.form["confirm_password"]):
+        flash("Please fill in all fields")
+        return redirect(url_for("register"))
+
+    if(story.check_user(request.form["user"])):
+        flash("User already exists")
+        return redirect(url_for("register"))
+
+    if(request.form["password"] != request.form["confirm_password"]):
+        flash("Passwords don't match")
+        return redirect(url_for("register"))
+
+    #story.add_user(request.form["user"], request.form["password"])
+    session["logged_in"] = request.form["user"]
+    return redirect(url_for("home"))
+
+#---------- Home Route ----------
+#Displays home page
+@app.route("/home")
 def home():
-    return render_template("index.html")
-
-
+    return render_template("home.html", user=session["logged_in"])
 
 if __name__ == "__main__":
     app.debug = True
---
+++ db.py
@@ -0,0 +1,57 @@
+import sqlite3
+DB_FILE="food.db"
+
+def createTable():
+    ''' creates the two main data tables for users and list of stories '''
+    db = sqlite3.connect(DB_FILE)
+    c = db.cursor()
+    command = "CREATE TABLE users (username TEXT, password TEXT)"
+    c.execute(command)
+
+    command = "CREATE TABLE favRest (username TEXT, restaurant TEXT)"
+    c.execute(command)
+
+    command = "CREATE TABLE favRec (username TEXT, recipe TEXT)"
+    c.execute(command)
+
+    command = "CREATE TABLE recRest (username TEXT, restaurant TEXT)"
+    c.execute(command)
+
+    command = "CREATE TABLE recRec (username TEXT, recipe TEXT)"
+    c.execute(command)
+
+    db.commit() #save changes
+    db.close()  #close database
+
+def add_user(username, password):
+    ''' insert credentials for newly registered user into database '''
+    db = sqlite3.connect(DB_FILE)
+    c = db.cursor()
+    c.execute("INSERT INTO users VALUES(?, ?)", (username, password))
+    db.commit() #save changes
+    db.close()  #close database
+
+def auth_user(username, password):
+    ''' authenticate a user attempting to log in '''
+    db = sqlite3.connect(DB_FILE)
+    c = db.cursor()
+
+    # user_info = c.execute("SELECT users.username, users.password FROM users WHERE username={} AND password={}".format(username, password))
+    for entry in c.execute("SELECT users.username, users.password FROM users"):
+        if(entry[0] == username and entry[1] == password):
+            db.close()
+            return True
+    db.close()
+    return False
+
+def check_user(username):
+    ''' check if a username has already been taken when registering '''
+    db = sqlite3.connect(DB_FILE)
+    c = db.cursor()
+
+    for entry in c.execute("SELECT users.username FROM users"):
+        if(entry[0] == username):
+            db.close()
+            return True
+    db.close()
+    return False
