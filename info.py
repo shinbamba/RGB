28cb3981eea7169c055b4b0d4c687f2dfb800450
Kenny <lli15@stuy.edu>
2018-11-29 14:08:05 -0500

    added getCities method in info

--- info.py
+++ info.py
@@ -7,9 +7,26 @@ yelpKey = "WZvWvogiVfxzNrpWipq63HV1fAJZKU4R-kDfewNhYQssKpX\
 jFCbbdwe0IGQW7Bh86cEEL7Xh5Yb8bgGOfgYaCJQ5BXt2-n0v4ymnkdL49ou1Ot1vNDBI__0egaj0W3Yx"
 usdaKey = "LSwsFBYAj6DAx8ChR6hy5420iIX8IyQCPoDMGt3G"
 
-zomatoUrl = request.Request("https://developers.zomato.com/api/v2.1")
-queryString = "/establishment/city_id=1"
-zomatoUrl.add_header("user-key", zomatoKey)
-zomatoUrl.add_header('User-Agent','Mozilla/5.0')
-data = json.loads(request.urlopen(zomatoUrl).read())
-print(data)
+
+#returns a dictionary of city names with their ids
+def getCites(city):
+    cityDict = dict()
+    cityString = "/cities?q=" + city
+    zomatoUrl = request.Request("https://developers.zomato.com/api/v2.1" + cityString)
+    zomatoUrl.add_header("user-key", zomatoKey)
+    zomatoUrl.add_header('User-Agent','Mozilla/5.0')
+    data = json.loads(request.urlopen(zomatoUrl).read())
+    for cityList in data["location_suggestions"]:
+        cityDict[cityList["name"]] = cityList["id"]
+    return cityDict
+
+# print(getCites("new"))
+
+
+# zomatoUrl = request.Request("https://developers.zomato.com/api/v2.1")
+# cityString = "/cities?q="
+# queryString = "/establishment/city_id=1"
+# zomatoUrl.add_header("user-key", zomatoKey)
+# zomatoUrl.add_header('User-Agent','Mozilla/5.0')
+# data = json.loads(request.urlopen(zomatoUrl).read())
+# print(data)
